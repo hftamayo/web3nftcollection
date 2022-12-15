62e6d66a9d96bdc2ec4aa021a2295c7c7af59556
hftamayo <hftamayo@gmail.com>
2022-12-14 19:42:23 -0600

    deploying the first version of the contract and frontend

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xd0376534caEFe18F930b222E6F1bB1Dc8e83a143";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-six-eta.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
